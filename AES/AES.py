3d35a4f6f52b63ef31f1d763341f125809fa5fbd
User <Willspri@gmail.com>
2024-02-05 12:02:16 +1300

    completed

--- AES/AES.py
+++ AES/AES.py
@@ -4,18 +4,35 @@ from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 
 #encrypt function 
-def encrypt():
+def encrypt(message, key):
     #generate key
+    cipher = AES.new(key, AES.MODE_CBC)
     #encrypt key
+    ciphertext = cipher.encrypt(pad(message.encode('utf-8'), AES.block_size))
     #encrypt message and add padding to make sure length is multiple of block size
+    
     #return key and encryption
-    return
+    return cipher.iv + ciphertext
 
 #decrypt function 
-def decrypt():
+def decrypt(ciphertext, key):
     #extract prepended blocksize 
+    iv = ciphertext[:AES.block_size]
     #Initialise cipher with provided key, mode of operation and initialisation vector
+    cipher = AES.new(key, AES.MODE_CBC, iv)
     #decrypt and unpad
-    return
+    decrypted_message = unpad(cipher.decrypt(ciphertext[AES.block_size:]), AES.block_size)
+    return decrypted_message.decode('utf-8')
+
  
 #print results
+key = get_random_bytes(32)
+message = "secret message"
+
+#encrypt
+ciphertext = encrypt(message, key)
+print("encrypted: ", ciphertext)
+
+#decrypt
+decrypted_message = decrypt(ciphertext, key)
+print("decrypted: ", decrypted_message)
