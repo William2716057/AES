7b39139b8b514a3dd82c1449b4cd2fce46fd3070
User <Willspri@gmail.com>
2024-02-05 11:54:05 +1300

    process

--- AES/AES.py
+++ AES/AES.py
@@ -1,7 +1,21 @@
 #imports 
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
 #encrypt function 
+def encrypt():
+    #generate key
+    #encrypt key
+    #encrypt message and add padding to make sure length is multiple of block size
+    #return key and encryption
+    return
 
 #decrypt function 
-
+def decrypt():
+    #extract prepended blocksize 
+    #Initialise cipher with provided key, mode of operation and initialisation vector
+    #decrypt and unpad
+    return
+ 
 #print results
